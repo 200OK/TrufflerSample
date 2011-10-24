4564e61a4ac953e611e6a45facaa855a22df4dd0
joelabrahamsson <mail@joelabrahamsson.com>
2011-10-25 00:44:04 +0200

    First commit.

---
+++ TrufflerSample/Controllers/RestaurantController.cs
@@ -0,0 +1,108 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Truffler;
+using TrufflerSample.Models;
+
+namespace TrufflerSample.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        IClient client;
+
+        public RestaurantController(IClient client)
+        {
+            this.client = client;
+        }
+
+        //
+        // GET: /Restaurant/
+
+        public ActionResult Index()
+        {
+            var restaurants = client.Search<Restaurant>().GetResult();
+            return View(restaurants);
+        }
+
+        //
+        // GET: /Restaurant/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Restaurant/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Restaurant/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, Restaurant newRestaurant)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newRestaurant);
+            }
+
+            client.Index(newRestaurant);
+
+            return RedirectToAction("Index");
+        }
+        
+        //
+        // GET: /Restaurant/Edit/5
+
+        public ActionResult Edit(string wikiurl)
+        {
+            return View(client.Get<Restaurant>(Server.UrlDecode(wikiurl)));
+        }
+
+        //
+        // POST: /Restaurant/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(string wikiurl, FormCollection collection, Restaurant restaurant)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(restaurant);
+            }
+
+            client.Index(restaurant);
+
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: /Restaurant/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Restaurant/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TrufflerSample/Models/SearchResult.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Truffler;
+
+namespace TrufflerSample.Models
+{
+    public class SearchResult
+    {
+        public SearchResult(SearchResults<SearchHit> results, string searchTerm)
+        {
+            Results = results;
+            SearchTerm = searchTerm;
+            CurrentPage = 1;
+        }
+
+        public SearchResults<SearchHit> Results { get; private set; }
+
+        public string SearchTerm { get; private set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<FacetResult> Facets { get; set; }
+    }
+
+    public class FacetLink
+    {
+        public string Text { get; set; }
+
+        public int Count { get; set; }
+
+        public string Url { get; set; }
+
+        public string CssClass { get; set; }
+    }
+
+    public class FacetResult
+    {
+        public FacetResult(string name, IEnumerable<FacetLink> links)
+        {
+            Name = name;
+            Links = links;
+        }
+
+        public string Name { get; private set; }
+
+        public IEnumerable<FacetLink> Links { get; private set; }
+    }
+}
